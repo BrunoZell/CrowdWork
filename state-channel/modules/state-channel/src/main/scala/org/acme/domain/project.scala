625643070dc54342b7c0ea6fa3a9c9cedbc1c68f
Bruno Zell <contact@brunozell.com>
2023-03-03 01:13:15 +0100

    add project domain models

---
+++ state-channel/modules/state-channel/src/main/scala/org/acme/domain/project.scala
@@ -0,0 +1,23 @@
+package org.acme.domain
+
+
+// Request sent and signed by the project creator to list a new project
+case class CreateProject(
+    id: String,
+    creatorWalletAddress: String, 
+    name: String, 
+    description: String, 
+    openPositions: Array[OpenPosition])
+
+trait OpenPosition
+case class ContractDeveloperPosition() extends OpenPosition
+case class MarketingPosition() extends OpenPosition
+case class DesignerPosition() extends OpenPosition
+
+// (most recent) state snapshot representing a single project
+case class ProjectSnapshot(
+    id: String,
+    creatorWalletAddress: String, 
+    name: String, 
+    description: String, 
+    openPositions: Array[OpenPosition])
