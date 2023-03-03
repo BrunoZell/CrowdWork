5c6488951ab7934952f3986311705a5b6299d390
Bruno Zell <contact@brunozell.com>
2023-03-03 02:07:27 +0100

    define builder messages

---
+++ state-channel/modules/state-channel/src/main/scala/org/acme/domain/Builder.scala
@@ -0,0 +1,25 @@
+package org.acme.domain
+
+trait Experience
+case class UpToOneYear() extends Experience
+case class OneToTwoYears() extends Experience
+case class TwoToFiveYears() extends  Experience
+case class FiveAndMoreYears() extends Experience
+
+case class Skill(skill: String, experience: Experience, interest: Integer)
+
+// Request sent and signed by the project creator to list a new project
+case class UpdateBuilderProfile(
+  builderWalletAddress: String,
+  name: String,
+  bio: String,
+  skillset: Array[Skill]
+)
+
+// (most recent) snapshot of a builders profile
+case class BuilderProfileSnapshot(
+  builderWalletAddress: String,
+  name: String,
+  bio: String,
+  skillset: Array[Skill]
+)
---
+++ state-channel/modules/state-channel/src/main/scala/org/acme/domain/Skills.scala
@@ -0,0 +1,27 @@
+package org.acme.domain
+
+object Skills {
+    val Developer = new Array[String](
+        "Javascript",
+        "Java",
+        "Solidity",
+        "Metamask",
+        "Bash",
+        "Scala",
+        "Vyper",
+        "EVM IL",
+    )
+    
+    val Designer = new Array[String](
+        "Figma",
+        "Photoshop",
+        "WebFlow",
+        "Bubble.io",
+    )
+    
+    val Marketing = new Array[String](
+        "Mailchimp",
+        "SEO",
+        "Print"
+    )
+}
