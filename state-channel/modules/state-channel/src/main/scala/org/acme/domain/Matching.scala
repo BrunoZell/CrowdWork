5388b2bed9d93545b63999281e0a16d22a5a2370
Bruno Zell <contact@brunozell.com>
2023-03-03 02:33:52 +0100

    cleanup

--- state-channel/modules/state-channel/src/main/scala/org/acme/domain/Matching.scala
+++ state-channel/modules/state-channel/src/main/scala/org/acme/domain/Matching.scala
@@ -1,10 +1,10 @@
 package org.acme.domain
 
-
 // Everybody can crawl through projects, builders, and investors and recommend a match.
-// It needs to come with a proof:
+// It needs to come with a proof that the closeness was indeed determined by a well-defined algorithm with the project + builder/investor used as an input
+    
 // for projects-builders: Skills of builder must be conceptually close to one or more open positons of the project
-// for projects-investors: Interests of investor must be conceptually close to the value propositon of the project
-
 case class ProjectBuilderMatchProposal(builderWalletAddress: String, projectId: String, proposerWalletAddress: String, closeness: Long, proof: String)
+
+// for projects-investors: Interests of investor must be conceptually close to the value propositon of the project
 case class ProjectInvestorMatchProposal(investorWalletAddress: String, projectId: String, proposerWalletAddress: String, closeness: Long, proof: String)
