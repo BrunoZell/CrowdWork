be85f90e1fcb439a40deee7e2b372cdcdcd8efa7
Bruno Zell <contact@brunozell.com>
2023-03-03 23:27:39 +0100

    add more skills

--- state-channel/modules/state-channel/src/main/scala/org/acme/domain/Skills.scala
+++ state-channel/modules/state-channel/src/main/scala/org/acme/domain/Skills.scala
@@ -1,15 +1,30 @@
 package org.acme.domain
 
 object Skills {
-    val Developer = new Array[String](
-        "Javascript",
-        "Java",
-        "Solidity",
+    val Fullstack = new Array[String](
+        "JavaScript",
         "Metamask",
-        "Bash",
-        "Scala",
+        "web3.js",
+        "dag4.js",
+        "Stargazer Wallet",
+        "Java",
+        "Vue.js",
+        "Angular",
+        "React",
+        "Javascript"
+    )
+    
+    val Contract = new Array[String](
+        "Solidity",
         "Vyper",
         "EVM IL",
+        "IEML",
+        "HGTP",
+        "Java",
+        "Hum",
+        "Noir",
+        "Scala",
+        "Rust",
     )
     
     val Designer = new Array[String](
